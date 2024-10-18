07b24369f61f8087602d667ba94d79386689f63f
Chris Griffing <cmgriffing@gmail.com>
2024-10-18 16:56:23 -0700

    feat: add github icon and link to header

--- playgrounds/app/src/components/Header.tsx
+++ playgrounds/app/src/components/Header.tsx
@@ -1,11 +1,15 @@
 import { useLocation } from '@solidjs/router'
 import { Button } from './ui/button'
+import { OcMarkgithub2 } from 'solid-icons/oc'
 
 export default function Nav() {
   return (
     <header class="flex flex-col">
       <nav class="flex flex-row gap-2 justify-end p-4">
-        <Button>Login/Signup</Button>
+        <a href="https://github.com/cmgriffing/lithium" target="_blank" rel="noreferrer">
+          <OcMarkgithub2 size={32} />
+        </a>
+        {/* <Button>Login/Signup</Button> */}
       </nav>
       <div class="flex flex-col items-center justify-center p-4">
         <h1 class="text-6xl font-bold">Lithium</h1>
