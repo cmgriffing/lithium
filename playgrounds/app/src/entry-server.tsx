8a45b84370abc339a1615df214d3f0351164633a
Chris Griffing <cmgriffing@gmail.com>
2024-10-19 15:44:29 -0700

    fix: add meta tags for favicons

--- playgrounds/app/src/entry-server.tsx
+++ playgrounds/app/src/entry-server.tsx
@@ -8,7 +8,12 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <meta name="apple-mobile-web-app-title" content="Lithium" />
+          <link rel="manifest" href="/site.webmanifest" />
           <script
             defer
             data-domain="lithium.cmgriffing.com"
